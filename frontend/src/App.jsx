0478e7f550039b08317a7261f570ddefcbff6321
Shorya1203 <shorya1203@gmail.com>
2025-09-13 15:33:32 +0530

    bug fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,11 @@ function App() {
 
   const [todos, setTodos] = useState([]) ; 
 
-  // fetch('http://localhost:3000/todos')
-  // .then(async function(res){
-  //   const json = await res.json() ;
-  //   setTodos(json.todos) ; 
-  // })
+  fetch('http://localhost:3000/todos')
+  .then(async function(res){
+    const json = await res.json() ;
+    setTodos(json.todos) ; 
+  })
 
   return (
     <div>
